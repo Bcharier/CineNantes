6f0207d8898fd917682d21874f6afd8c94b4a08a
baptiste <charier.ba@gmail.com>
2025-11-11 10:21:16 +0100

    remove theaters from local storage and fix responsive css

--- static/js/filters.js
+++ static/js/filters.js
@@ -1,32 +1,12 @@
 const theaterFilters = document.querySelectorAll('.theater-filter');
 
-// Restore selected theaters from localStorage
-const savedTheaters = JSON.parse(localStorage.getItem('selectedTheaters') || '[]');
-for (const button of theaterFilters) {
-    if (savedTheaters.includes(button.textContent)) {
-        button.classList.add('active');
-    }
-}
-
-// Initial filter on page 
-filterShowtimesByTheater();
-
-// Add event listeners to theater filter buttons
 for (const button of theaterFilters) {
     button.addEventListener('click', () => {
         button.classList.toggle('active');
-
-         // Save selected theaters to localStorage
-        const activeTheaters = Array.from(theaterFilters)
-            .filter(btn => btn.classList.contains('active'))
-            .map(btn => btn.textContent);
-        localStorage.setItem('selectedTheaters', JSON.stringify(activeTheaters));
-        
         filterShowtimesByTheater();
     });
 }
 
-// Function to filter showtimes based on selected theaters
 function filterShowtimesByTheater() {
     const activeTheaters = Array.from(theaterFilters)
         .filter(button => button.classList.contains('active'))
@@ -59,4 +39,7 @@ const filterContainer = document.querySelector('.mobile-filter-container');
 const filterButton = document.querySelector('.mobile-filter-button');
 filterButton.addEventListener('click', () => {
     filterContainer.classList.toggle('visible');
+    const movieContainer = document.querySelector('.container_infoFilm')
+    movieContainer.classList.toggle('hidden');
+
 });
